9117808da81b762d8dfcc45472915fa700053280
gringrape <gringrape200@gmail.com>
2020-05-26 06:09:33 +0900

    Completed module

--- src/services.js
+++ src/services.js
@@ -1,6 +1,6 @@
 export function fetchWithTimeout(delay) {
   // delay 시간이 지난뒤에 pending 에서 resolved 상태로 이행하는 프라미스 생성
   return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  })
+    setTimeout(resolve(), delay);
+  });
 }
